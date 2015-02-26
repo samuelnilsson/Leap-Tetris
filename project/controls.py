1c09bcdbaf1eec350ddbf6c4b7009a23f25688f2
Anton Niklasson <niklasson.anton@gmail.com>
2015-02-26 14:05:33 +0100

    Trying to fix the libs.

--- project/controls.py
+++ project/controls.py
@@ -1,5 +1,8 @@
 import sys, inspect, thread, time
 sys.path.insert(0, "lib")
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
