f3fde848e58340240bc0398819f71d18f94db118
samuelnilsson <samni698@gmail.com>
2015-03-03 20:26:19 +0100

    Now the button lights up when the mouse is hold over it, to show that it is clickable.

--- project/mode_switcher.py
+++ project/mode_switcher.py
@@ -6,6 +6,7 @@ class Mode_switcher:
     def __init__(self):
         self._leap_mode = False
         self._font = pygame.font.SysFont('Arial', 18)
+        self._is_mouse_over_button = False
         self.POSITION_X = 10
         self.POSITION_Y = 10
         self.WIDTH = 50
@@ -17,8 +18,12 @@ class Mode_switcher:
         button = pygame.Rect(self.POSITION_X, self.POSITION_Y,
                              self.WIDTH, self.HEIGHT)
         color = self.RED
+        if self._is_mouse_over_button:
+            color = (255, 100, 100)
         if self._leap_mode:
             color = self.GREEN
+            if self._is_mouse_over_button:
+                color = (150, 255, 150)
         pygame.draw.rect(surface, color, button)
         text_surface = self._font.render('Leap', True, (0, 0, 0))
         surface.blit(text_surface, (self.POSITION_X + 5, self.POSITION_Y))
@@ -27,10 +32,19 @@ class Mode_switcher:
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             self.on_click_event(pos)
+        elif event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            self.on_mouse_movement(pos)
 
     def on_click_event(self, pos):
+        if self.is_position_on_button(pos):
+            self._leap_mode = not self._leap_mode
+
+    def on_mouse_movement(self, pos):
+        self._is_mouse_over_button = self.is_position_on_button(pos)
+
+    def is_position_on_button(self, pos):
         (x, y) = pos
         on_button_x = x < self.POSITION_X + self.WIDTH and x > self.POSITION_X
         on_button_y = y < self.POSITION_Y + self.HEIGHT and y > self.POSITION_Y
-        if on_button_x and on_button_y:
-            self._leap_mode = not self._leap_mode
+        return on_button_x and on_button_y
